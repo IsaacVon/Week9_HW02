b0af9fd086dde99c730111155d7e3b308fe4b7c7
Isaac Householder <isaachouseholder@gmail.com>
2020-06-18 09:52:33 -0700

    Update server.js
    - connected to heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://user:rootroot1@ds021711.mlab.com:21711/heroku_d5zd9gg3", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
